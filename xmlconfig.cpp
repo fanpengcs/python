499805849592c68d1a2d5cfab1a8e261a67ee484
fanpeng_company <fanpeng@ztgame.com>
2019-11-15 11:45:52 +0800

    xmlbind update

--- xmlconfig.cpp
+++ xmlconfig.cpp
@@ -5,6 +5,7 @@ namespace XmlConfig {
 
 	static std::map<std::string, bool (*)(const XmlConfig::ReadXmlFunc &)> fileResetMap = std::map<std::string, bool (*) (const XmlConfig::ReadXmlFunc &)>();
 
+	static example_t *_example = NULL;
 
 	bool existConfig(const std::string &fileName) {
 		return fileResetMap.find(fileName) != fileResetMap.end();
@@ -20,6 +21,34 @@ namespace XmlConfig {
 	void init(const XmlConfig::ReadXmlFunc &func) {
 		fileResetMap.clear();
 		Fir::XMLParser xml;
+		if (_example) {
+		delete _example;
+			_example = NULL;
+		}
+		_example = new example_t;
+		_example->load(xml, func(xml, "example.xml"));
+		zLogger::debug("[XML配置], 加载 example.xml 成功");
+		fileResetMap.insert(std::make_pair("example", resetexample));
+	}
+
+	const example_t &example() {
+		if (NULL == _example)
+		{
+			zLogger::debug("[XML配置], 加载 ExampleSecond.xml 未初始化");
+			_example = new example_t();
+		}
+		return *_example;
+	}
+	bool resetexample(const XmlConfig::ReadXmlFunc &func) {
+		Fir::XMLParser xml;
+		if (_example) {
+			delete _example;
+			_example = NULL;
+		}
+		_example = new example_t;
+		_example->load(xml, func(xml, "example.xml"));
+		zLogger::debug("[XML配置] 加载 example.xml 成功");
+		return true;
 	}
 
 }
--- xmlconfig_define.h
+++ xmlconfig_define.h
@@ -42,6 +42,126 @@ namespace XmlConfig {
 		}
 	}
 
+	struct example_t {
+		public:
+			struct structItem_t {
+				public:
+					struct item_t {
+						public:
+							void load(const Fir::XMLParser &xml, const Fir::XMLParser::Node *node);
+							const Fir::VarType &itemid()const { return _itemid; }
+							const Fir::VarType &itemvar1()const { return _itemvar1; }
+							const Fir::VarType &itemvar2()const { return _itemvar2; }
+
+						private:
+							Fir::VarType _itemid;
+							Fir::VarType _itemvar1;
+							Fir::VarType _itemvar2;
+					};
+
+				public:
+					void load(const Fir::XMLParser &xml, const Fir::XMLParser::Node *node);
+					const std::map< Fir::VarType, item_t > &exampleMap()const { return _exampleMap; }
+					const Fir::VarType &item1()const { return _item1; }
+					const Fir::VarType &item2()const { return _item2; }
+
+				private:
+					std::map< Fir::VarType, item_t > _exampleMap;
+					Fir::VarType _item1;
+					Fir::VarType _item2;
+			};
+
+			struct structName_t {
+				public:
+					struct example_t {
+						public:
+							struct mapitem_t {
+								public:
+									void load(const Fir::XMLParser &xml, const Fir::XMLParser::Node *node);
+									const Fir::VarType &id()const { return _id; }
+									const Fir::VarType &va()const { return _va; }
+									const Fir::VarType &vb()const { return _vb; }
+									const Fir::VarType &vc()const { return _vc; }
+
+								private:
+									Fir::VarType _id;
+									Fir::VarType _va;
+									Fir::VarType _vb;
+									Fir::VarType _vc;
+							};
+
+						public:
+							void load(const Fir::XMLParser &xml, const Fir::XMLParser::Node *node);
+							const std::map< Fir::VarType, mapitem_t > &exMap()const { return _exMap; }
+							const Fir::VarType &exam1()const { return _exam1; }
+							const Fir::VarType &exam2()const { return _exam2; }
+							const Fir::VarType &exam3()const { return _exam3; }
+
+						private:
+							std::map< Fir::VarType, mapitem_t > _exMap;
+							Fir::VarType _exam1;
+							Fir::VarType _exam2;
+							Fir::VarType _exam3;
+					};
+
+					struct item_t {
+						public:
+							void load(const Fir::XMLParser &xml, const Fir::XMLParser::Node *node);
+							const Fir::VarType &itemid()const { return _itemid; }
+							const Fir::VarType &itemvar1()const { return _itemvar1; }
+							const Fir::VarType &itemvar2()const { return _itemvar2; }
+
+						private:
+							Fir::VarType _itemid;
+							Fir::VarType _itemvar1;
+							Fir::VarType _itemvar2;
+					};
+
+					struct structIt_t {
+						public:
+							void load(const Fir::XMLParser &xml, const Fir::XMLParser::Node *node);
+							const Fir::VarType &it1()const { return _it1; }
+							const Fir::VarType &it2()const { return _it2; }
+							const Fir::VarType &it3()const { return _it3; }
+
+						private:
+							Fir::VarType _it1;
+							Fir::VarType _it2;
+							Fir::VarType _it3;
+					};
+
+				public:
+					void load(const Fir::XMLParser &xml, const Fir::XMLParser::Node *node);
+					const std::map< Fir::VarType, item_t > &exampleMap()const { return _exampleMap; }
+					const std::vector< example_t > &exampleVector()const { return _exampleVector; }
+					const std::vector< example_t > &firstVector()const { return _firstVector; }
+					const structIt_t &structIt()const { return _structIt; }
+					const Fir::VarType &var1()const { return _var1; }
+					const Fir::VarType &var2()const { return _var2; }
+
+				private:
+					std::map< Fir::VarType, item_t > _exampleMap;
+					std::vector< example_t > _exampleVector;
+					std::vector< example_t > _firstVector;
+					structIt_t _structIt;
+					Fir::VarType _var1;
+					Fir::VarType _var2;
+			};
+
+		public:
+			void load(const Fir::XMLParser &xml, const Fir::XMLParser::Node *node);
+			const structItem_t &structItem()const { return _structItem; }
+			const structName_t &structName()const { return _structName; }
+			const Fir::VarType &version1()const { return _version1; }
+			const Fir::VarType &version2()const { return _version2; }
+
+		private:
+			structItem_t _structItem;
+			structName_t _structName;
+			Fir::VarType _version1;
+			Fir::VarType _version2;
+	};
+
 }
 
 #endif
